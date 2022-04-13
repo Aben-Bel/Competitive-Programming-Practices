e3f6724792041d4a141fc46c2e58a1ab897775fe
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-04-13 14:03:12 +0300

    Time: 5 ms (89.59%), Space: 43.1 MB (94.18%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,40 +1,37 @@
 class Solution {
+    
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        HashMap<Integer, List<Integer>> adjList = new HashMap<>();
-        
-        for(int[] edge : prerequisites){
-            indegree[edge[0]]+=1;
-            List<Integer> neighbours = adjList.getOrDefault(edge[1], new ArrayList<Integer>());
-            neighbours.add(edge[0]);
-            adjList.put(edge[1], neighbours);
-        }
-        
+        HashMap<Integer, List<Integer>> outDegree = new HashMap<>();
+        int[] inDegree = new int[numCourses];
         Queue<Integer> queue = new LinkedList<>();
+        
+        for(int i=0;i<prerequisites.length;i++){
+            int src = prerequisites[i][1];
+            int dest = prerequisites[i][0];
+            List<Integer> lst = outDegree.getOrDefault(src, new ArrayList<>());
+            lst.add(dest);
+            outDegree.put(src, lst);
+            inDegree[dest]+=1;
+        }
         for(int i=0;i<numCourses;i++){
-            if(indegree[i] == 0){
-                queue.offer(i);
-            }
+            if(inDegree[i] == 0) queue.offer(i);
         }
         
-        int[] topSort = new int[numCourses];
-        int i = 0;
+        
+        int[] result = new int[numCourses];
+        int i=0;
         while(!queue.isEmpty()){
-            int cur = queue.remove();
-            topSort[i++] = cur;
-            if (adjList.containsKey(cur)) {
-                for(int node : adjList.get(cur)){
-                    indegree[node]-=1;
-                    if(indegree[node]==0){
-                        queue.offer(node);
-                    }
-                }
+            int node = queue.remove();
+            result[i++] = node;
+            
+            for(int nei : outDegree.getOrDefault(node, new ArrayList<>())){
+                inDegree[nei]-=1;
+                if(inDegree[nei] == 0) queue.offer(nei);
             }
         }
         
-        if(i==numCourses){
-            return topSort;
-        }
+        if(i==numCourses) return result;
+        
         return new int[0];
     }
 }
