d2b4bf189bbf47e22400df73d8f80399e1dff174
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-05-06 14:12:59 +0300

    Time: 55 ms (33.93%), Space: 14 MB (12.46%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -3,24 +3,10 @@ class Solution:
         if len(strs) == 0: return ""
         if len(strs) == 1: return strs[0]
         
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for j in range(1,len(strs)):
+                if i==len(strs[j]) or char != strs[j][i]:
+                    return strs[0][:i]
+        return strs[0]
         
-        commonPrefix = self.getCommon(strs[0], strs[1])
-        for i in range(2, len(strs)):
-            commonPrefix = self.getCommon(strs[i], commonPrefix)
-            if commonPrefix == "":
-                return commonPrefix
-        
-        return commonPrefix
-            
-            
-    def getCommon(self, str1, str2):
-        commonPrefix = ""
-        i = 0
-        j = 0
-        while i<len(str1) and j<len(str2):
-            if str1[i] != str2[j]:
-                break
-            i+=1
-            j+=1
-        commonPrefix = str1[:i]
-        return commonPrefix
