3bf1cfc97f5c7b05c137661beeee70ea3b05b4ad
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-05-13 07:33:01 +0300

    Time: 92 ms (80.25%), Space: 20.8 MB (79.79%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,11 +1,15 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        bitCounts = []
-        for i in range(n+1):
-            count = 0
-            while i>0:
-                if i&1==1:
-                    count+=1
-                i >>= 1
-            bitCounts.append(count)
+        bitCounts = [0]*(n+1)
+        
+        powerTwo = 1
+        current = 0
+        for i in range(1,n+1):
+            if i == powerTwo:
+                bitCounts[i] = 1
+                powerTwo <<= 1
+                current = 1
+            else:
+                bitCounts[i] = bitCounts[current] + 1
+                current += 1
         return bitCounts
