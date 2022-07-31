6e0aab496941ab7046f9049cfab5dff1457a85e3
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-07-31 07:06:30 +0300

    Time: 556 ms (66.67%), Space: 27.3 MB (83.33%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.py
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.py
@@ -1,35 +1,9 @@
 class Solution:
     def maximumGroups(self, grades: List[int]) -> int:
-        # [10,6,12,7,3,5]
-        # [3,5,6,7,10,12]
-        # [0,3,8,14,]
-        grades.sort()
-        # prefix = [0]*len(grades)
-        # for i in range(len(grades)):
-        #     if i==0:
-        #         prefix[i] = grades[i]
-        #         continue
-        #     prefix[i] = prefix[i-1] + grades[i]
-        # prefix = [0] + prefix
-        # n = 1
-        # prev = 0
-        # s = 0
-        # count = 1
-        # for i in range(len(grades)):
-        #     if n - s - i == 0:
-        #         val = prefix[i] - prefix[s]
-        #         if val == prev:
-        #             return count
-        #         n+=1
-        #         s=i
-        #         count+=1
-        # return count
         n = 1
         s = 0
         prev = 0
         count = 0
-        # [8,8]
-        # [1,2,3,4,5]
         while s+n<=len(grades):
             count+=1
             s = s+n
