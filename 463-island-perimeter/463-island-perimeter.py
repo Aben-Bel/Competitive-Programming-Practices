85af566cd14961283fec0a7b3c690e3b669f224e
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-08-19 09:07:09 +0300

    Time: 2084 ms (5.03%), Space: 21.4 MB (18.11%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -1,5 +1,17 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        def getNeis(i,j,grid):
+            N=len(grid)
+            M= len(grid[0])
+            directions = [[1,0],[0,1],[-1,0],[0,-1]]
+            inbound = lambda i,j : 0<= i <N and 0<= j <M
+            result = []
+            for xv, yv in directions:
+                nx = xv + i 
+                ny = yv + j
+                if inbound(nx,ny):
+                    result.append((nx,ny))
+            return result
         
         def perimeter(i,j, grid):
             N=len(grid)
@@ -18,9 +30,22 @@ class Solution:
             return result
             
             
-        totalPerimeter = 0
+        self.totalPerimeter = 0
+        def dfs(i,j,grid,visited):
+            if (i,j) in visited:
+                return 0
+            if grid[i][j] == 0:
+                return 0
+            
+            visited.add((i,j))
+            s = perimeter(i,j,grid)
+            for x,y in getNeis(i,j,grid):
+                s += dfs(x,y,grid,visited)
+            return s
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    totalPerimeter += perimeter(i, j, grid)
-        return totalPerimeter
+                    return dfs(i,j,grid,set())
+                    
+        return 0
