450fc0e192bc73f50f345ceee4cb8ad07ff343f7
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-09-14 16:24:10 +0300

    Time: 799 ms (26.36%), Space: 13.9 MB (9.51%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,13 +1,6 @@
 class Solution:
-    def __init__(self):
-        self.mem = {}
-        
     def fib(self, n: int) -> int:
         if n==1 or n==0:
             return n
         
-        if n in self.mem:
-            return self.mem[n]
-        
-        self.mem[n] = self.fib(n-1) + self.fib(n-2)
-        return self.mem[n]
+        return self.fib(n-1) + self.fib(n-2)
