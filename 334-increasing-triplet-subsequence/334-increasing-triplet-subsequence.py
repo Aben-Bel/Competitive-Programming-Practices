28b08dbd35a211da18bd4f16c19db946382b58c8
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-09-27 09:11:33 +0300

    Time: 2145 ms (5.00%), Space: 24.5 MB (81.41%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -1,15 +1,30 @@
 class Solution:
-    def increasingTriplet(self, nums: List[int]) -> bool:     
-        one = two = float('inf')
+    def increasingTriplet(self, nums: List[int]) -> bool:    
+        if len(nums) < 3:
+            return False
         
-        for num in nums:
-            if one > num:
-                one = num
-            
-            if two > num and one < num:
-                two = num
-            
-            if two < num:
-                return True
+        def getIndex(val, triplets):
+            left = 0
+            right = len(triplets) -1
+            best = -1
+            while left <= right:
+                mid = (left + right)//2
+                if triplets[mid]  > val:
+                    best = mid
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            if best!=-1 and val != triplets[best-1]: return best
+            return -1
         
+        triplets = [nums[0]]
+        for i in range(1, len(nums)):
+            if triplets[-1] < nums[i]:
+                triplets.append(nums[i])
+                if len(triplets) == 3:
+                    return True
+            else:
+                j = getIndex(nums[i], triplets)
+                if j != -1: triplets[j] = nums[i]
         return False
+                
