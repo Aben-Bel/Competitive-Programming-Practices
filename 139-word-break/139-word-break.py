abdc0df6186ac5ce68bcee5a94a777fd4b326027
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-09-28 12:58:50 +0300

    Time: 82 ms (24.10%), Space: 14.2 MB (25.73%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,25 +2,29 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         N = len(s)
         _dict = set(wordDict)
-        dp = [False] * (N + 1)
-        dp[N] = True
+#         dp = [False] * (N + 1)
+#         dp[N] = True
         
-        for i in range(N - 1, -1, -1):
+#         for i in range(N - 1, -1, -1):
+#             for word in _dict:
+#                 n = len(word)
+#                 if i + n <= N:
+#                     dp[i] |= s[i: i + n] in _dict and dp[i + n]
+#             # for x in range(i, N):
+#             #     dp[i] |= s[i: x + 1] in _dict and dp[x + 1]
+#         return dp[0]
+        
+        
+        @cache
+        def dp(i):
+            if i > len(s):
+                return False
+            if i == len(s):
+                return True
+            tmp = False
             for word in _dict:
                 n = len(word)
-                if i + n <= N:
-                    dp[i] |= s[i: i + n] in _dict and dp[i + n]
-            # for x in range(i, N):
-            #     dp[i] |= s[i: x + 1] in _dict and dp[x + 1]
-        return dp[0]
-        
-        
-        # @cache
-        # def dp(i):
-        #     if i >= len(s):
-        #         return True
-        #     tmp = False
-        #     for x in range(i, len(s)):
-        #         tmp |= s[i: x + 1] in _dict and dp(x + 1)
-        #     return tmp
-        # return dp(0)
+                tmp |= s[i: i + n] in _dict and dp(i + n)
+
+            return tmp
+        return dp(0)
