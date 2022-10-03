0254eaed22974aa485e86ea2357b1ee39ad80cfb
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-10-03 10:48:57 +0300

    Time: 1514 ms (7.72%), Space: 15.8 MB (99.11%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -8,7 +8,7 @@ class Solution:
             for a,b,w in times:
                 if delay[a-1] != float('inf') and delay[a-1]+w < delay[b-1]:
                     delay[b-1] = delay[a-1] + w
-        print(delay)
+        # print(delay)
         res = max(delay)
         return res if res != float('inf') else -1
         
