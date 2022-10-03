5d395ae5eaf7d6857c21594bddff75abb5dd2643
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-10-03 12:25:54 +0300

    Time: 4404 ms (5.02%), Space: 15.8 MB (97.06%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,28 +1,16 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = defaultdict(list)
-        for a, b, w in times:
-            graph[a].append((b,w))
+        matrix = [[float('inf') if i!=j else 0 for j in range(n)] for i in range(n)]
         
-        minHeap = [(0,k)]
-        delay = [float('inf')]*(n+1)
-        delay[k] = 0
-        visited = set()
-        while minHeap:
-            time, node = heapq.heappop(minHeap)
-                
-            if node in visited:
-                continue
-            
-            visited.add(node)
-            for toNode, cost in graph[node]:
-                if delay[toNode] > delay[node] + cost:
-                    delay[toNode] = delay[node] + cost
-                    heapq.heappush(minHeap, (delay[toNode],toNode))
-                
-        res = max(delay[1:])
-        return res if res != float('inf') else -1 
+        for a,b,w in times:
+            matrix[a-1][b-1] = w
         
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                for l in range(n):
+                    matrix[j][l] = min(matrix[j][l], matrix[j][i]+matrix[i][l])
+        res = max(matrix[k-1])
+        return res if res!=float('inf') else -1
         
         
             
