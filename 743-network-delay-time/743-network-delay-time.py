067202388130830e09c460dd03030b0550dcf060
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-10-03 11:57:20 +0300

    Time: 1270 ms (16.15%), Space: 16.8 MB (11.36%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,20 +1,28 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        for a, b, w in times:
+            graph[a].append((b,w))
         
-        delay = [float('inf')]*n
+        minHeap = [(0,k)]
+        delay = [float('inf')]*(n+1)
+        delay[k] = 0
+        visited = set()
+        while minHeap:
+            time, node = heapq.heappop(minHeap)
+                
+            if node in visited:
+                continue
+            
+            visited.add(node)
+            for toNode, cost in graph[node]:
+                if delay[toNode] > delay[node] + cost:
+                    delay[toNode] = delay[node] + cost
+                    heapq.heappush(minHeap, (delay[toNode],toNode))
+                
+        res = max(delay[1:])
+        return res if res != float('inf') else -1 
         
-        delay[k-1] = 0
-        for i in range(n-1):
-            noChange = True
-            for a,b,w in times:
-                if delay[a-1] != float('inf') and delay[a-1]+w < delay[b-1]:
-                    delay[b-1] = delay[a-1] + w
-                    noChange = False
-            if noChange:
-                break
-
-        res = max(delay)
-        return res if res != float('inf') else -1
         
         
             
