b9c01aac61a1b72c89e37b7ae690c0eb8887d806
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-10-14 05:03:03 +0300

    Time: 200 ms (5.17%), Space: 18.4 MB (27.02%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -1,54 +1,36 @@
 class Solution:
+    def predecessor(self, root):
+        node = root.left
+        while node and node.right:
+            node = node.right
+        return node.val if node else -1
+
+    def successor(self, root):
+        node = root.right
+        while node and node.left:
+            node = node.left
+        return node.val if node else -1
+    
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        """
-            first search for the node 
-            when i find it, 
-                i will return its left child and right child
-            otherwise,
-                i return null
-            
-            if as a parent, i recieve left,right child
-                first i will make my left child, the right child i recieve
-                second, i will iterate through the right child until i find null and then attach the left child 
-                finally, i will return null
-            otherwise
-                i will return null                
-        """
+        
+        
         if not root:
-            return root
+            return None
         
-        if root.val == key:
-            if root.right:
-                cur = root.right
-                while cur and cur.left!=None:
-                    cur = cur.left
-                cur.left = root.left
-                return root.right
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not (root.left or root.right):
+                return None
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
             else:
-                return root.left
-        
-        root.left = self.deleteNode(root.left, key)
-        root.right = self.deleteNode(root.right, key)
-        
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
         return root
-        
-        
-        
-        
-#         if root.val == key:
-#             return (root.left, root.right)
-        
-#         leftLeft, leftRight = self.deleteNode(root.left, key)
-#         rightLeft, rightRight = self.deleteNode(root.right, key)
-        
-#         if leftLeft or leftRight:
-            
-#         elif rightLeft or rightRight:
-            
-            
-#         return None, None
-            
-            
             
             
             
