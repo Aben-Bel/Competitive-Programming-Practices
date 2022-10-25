6c4fd09c3f810bedaee289fb0012dfc358147c77
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-10-25 09:42:14 +0300

    Time: 3838 ms (31.06%), Space: 497.2 MB (5.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -5,10 +5,13 @@ class Solution:
             return False
         
         @lru_cache(None)
-        def dp(i, bucket1, bucket2):            
-            if i>=len(nums):
-                return bucket1 == bucket2
+        def dp(i, current):
+            if current == total//2:
+                return True
             
-            return dp(i+1, bucket1+nums[i],bucket2) or dp(i+1, bucket1, bucket2+nums[i])
+            if i>=len(nums):
+                return False
+            
+            return dp(i+1, current + nums[i]) or dp(i+1, current)
         
-        return dp(0, 0, 0)
+        return dp(0, 0)
