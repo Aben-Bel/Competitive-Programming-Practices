35ca9be145b912f59491f0ecc619c1895fb4d4cc
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2022-11-02 16:53:18 +0300

    Time: 147 ms (35.71%), Space: 16.5 MB (5.05%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,39 +1,52 @@
 class Solution:
-    def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        numOfObstacles = 0
-        startNode = None
-        n,m = len(grid), len(grid[0])
+    def __init__(self):
+        self.grid = None
+        self.full = None
         
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+        startNode = None
+        n, m = len(grid), len(grid[0])
+        path = 0
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == -1:
-                    numOfObstacles += 1
+                val = self.getIndex(i,j)
+                if grid[i][j] != -1:
+                    path = (path | (1<<(val)))
+                    
                 if grid[i][j] == 1:
                     startNode = (i,j)
-        
-        return self.backtrack(startNode[0], startNode[1], grid, set([startNode]), numOfObstacles)
+        self.full = path
+        val = self.getIndex(startNode[0], startNode[1])
+        return self.backtrack(startNode[0], startNode[1], 1<<val)
     
-    def backtrack(self, i, j, grid, path, numOfObstacles):
-        if grid[i][j] == 2:
-            if len(path) == (len(grid)*len(grid[0]) - numOfObstacles):
+    def getIndex(self,i,j):
+        n = len(self.grid[0])
+        return n*i + j
+
+    @lru_cache(None)
+    def backtrack(self, i, j, path):
+        if self.grid[i][j] == 2:
+            if path == self.full:
                 return 1
             else:
                 return 0
         
-        if grid[i][j] == -1:
+        if self.grid[i][j] == -1:
             return 0
         
         count = 0
         for dx, dy in [[1,0],[0,1],[-1,0],[0,-1]]:
             ni = i + dx
             nj = j + dy
-            if 0<=ni<len(grid) and 0<=nj<len(grid[0]):
-                if (ni,nj) in path:
+            if 0<=ni<len(self.grid) and 0<=nj<len(self.grid[0]):
+                val = self.getIndex(ni,nj)
+                if path & (1<<(val)):
                     continue
-                    
-                path.add((ni,nj))
-                count += self.backtrack(ni,nj,grid,path,numOfObstacles)
-                path.remove((ni,nj))
+                prev = path
+                path = (path | (1<<(val)))
+                count += self.backtrack(ni,nj,path)
+                path = prev
         
         return count
         
